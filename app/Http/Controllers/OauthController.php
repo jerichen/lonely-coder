4a027fe05f7e09d762180d0abc539608a7f1d1af
jerichen <jerichen@cw.com.tw>
2019-01-07 23:04:39 +0800

    for test

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -10,12 +10,23 @@ class OauthController extends Controller
 {
     public function oauth (Request $request)
     {
-        $url = env('KKBOX_OAUTH_BASE_URL') . '?response_type=code&state=0001&redirect_uri=' . env('KKBOX_OAUTH_REDIRECT_URL') . '&client_id=' .env('KKBOX_CLIENT_ID');
-        return redirect($url);
+//        $url = env('KKBOX_OAUTH_BASE_URL') . '?response_type=code&state=0001&redirect_uri=' . env('KKBOX_OAUTH_REDIRECT_URL') . '&client_id=' .env('KKBOX_CLIENT_ID');
+//        return redirect($url);
+
+        $client = new Client();
+        $client->request('GET', env('KKBOX_ACCESS_TOKEN_URL'), [
+            'form_params' => [
+                'state' => '0001',
+                'client_id' => env('KKBOX_CLIENT_ID'),
+                'response_type' => 'code',
+                'redirect_uri' => env('KKBOX_OAUTH_REDIRECT_URL')
+            ]
+        ]);
     }
 
     public function oauthCallback(Request $request)
     {
+        dd($request->all());
         $code = $request->get('code');
 
         $client = new Client();
